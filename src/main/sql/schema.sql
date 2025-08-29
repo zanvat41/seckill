9d5273f40c681346ebe56d0d5149725ba26e8add
zanvat41 <fslinzhe@hotmail.com>
2025-08-29 18:23:22 +0800

    Initial commit

---
+++ src/main/sql/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE seckill;
+
+USE seckill;
+
+CREATE TABLE seckill(
+	`seckill_id` BIGINT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(120) NOT NULL,
+	`number` INT NOT NULL,
+	`start_time` TIMESTAMP NOT NULL,
+	`end_time` TIMESTAMP NOT NULL,
+	`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`seckill_id`),
+	KEY `idx_start_time` (`start_time`),
+	KEY `idx_end_time` (`end_time`),
+	KEY `idx_create_time` (`create_time`)
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
+
+INSERT INTO
+	seckill(name, number, start_time, end_time)
+VALUES
+	('$1000 iphone17', 100, '2015-11-01 00:00:00', '2015-11-02 00:00:00'),
+	('$500 ipad mini', 200, '2015-11-01 00:00:00', '2015-11-02 00:00:00'),
+	('$300 Transformers Optimus Prime', 300, '2015-11-01 00:00:00', '2015-11-02 00:00:00'),
+	('$200 Leetcode Membership', 400, '2015-11-01 00:00:00', '2015-11-02 00:00:00');
+
+CREATE TABLE success_killed(
+	`seckill_id` BIGINT NOT NULL,
+	`user_phone` BIGINT NOT NULL,
+	`state` TINYINT NOT NULL DEFAULT -1 COMMENT 'status：-1 invalid，0 success，1 paid，2 sent',
+	`create_time` TIMESTAMP NOT NULL,
+	PRIMARY KEY (`seckill_id`, `user_phone`),
+	KEY `idx_create_time` (`create_time`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,46 @@
+package org.seckill.dao;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void queryById() {
+        long id = 1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void queryAll() {
+        List<Seckill> seckills = seckillDao.queryAll(0, 100);
+        for (Seckill seckill : seckills) {
+            System.out.println(seckill);
+        }
+    }
+
+    @Test
+    public void reduceNumber() {
+        Date killTime = new Date();
+        int updateCount = seckillDao.reduceNumber(1000L, killTime);
+        System.out.println("updateCount=" + updateCount);
+        assertEquals(0, updateCount);
+    }
+}
